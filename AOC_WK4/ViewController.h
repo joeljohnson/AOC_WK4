75d9ac7a7ef9138ea734506d507485c10b200710
Joel Johnson <joelj@fullsail.edu>
2012-11-13 18:32:26 -0800

    Finished Week 4

--- AOC_WK4/ViewController.h
+++ AOC_WK4/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    UITextField *usernameText;
+    UILabel *pleaseEnterLabel;
+    UILabel *creatorInfo;
+}
+- (void)onClick:(NSString*)showThis;
 
 @end
--- AOC_WK4/ViewController.m
+++ AOC_WK4/ViewController.m
@@ -16,8 +16,132 @@
 
 - (void)viewDidLoad
 {
+
+    self.view.backgroundColor = [UIColor whiteColor];
+//Create a UILabel near the top of your screen with the text "Username:" in it.
+    UILabel *usernameLabel = [[UILabel alloc]initWithFrame:CGRectMake(10.0f, 10.0f, 90.0f, 30.0f)];
+    		if (usernameLabel != nil)
+            {
+                usernameLabel.text = @"Username:";
+                
+            }
+    [self.view addSubview:usernameLabel];
+//Create a UITextField to the right of the username label
+    usernameText = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
+    if (usernameText != nil)
+    {
+        [usernameText setBorderStyle:UITextBorderStyleRoundedRect];
+        [self.view addSubview:usernameText];
+
+    }
+//Create a rounded rectangle UIButton of any color under the UITextField with the text "Login" on it.
+    UIButton *usernameBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (usernameBtn != nil)
+    {
+        usernameBtn.frame = CGRectMake(10.0f, 50.0f, 300.0f, 30.0f);
+        usernameBtn.tintColor = [UIColor grayColor];
+        usernameBtn.tag = 0;
+        [usernameBtn setTitle:@"Login" forState:UIControlStateNormal];
+        [usernameBtn addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.view addSubview:usernameBtn];
+    }
+    
+//Create another UILabel beneath with the default text "Please Enter Username".
+        pleaseEnterLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 100.0f, 320.0f, 120.0f)];
+        if(pleaseEnterLabel != nil)
+        {
+            pleaseEnterLabel.backgroundColor = [UIColor lightGrayColor];
+            pleaseEnterLabel.text = @"Please Enter Username";
+            pleaseEnterLabel.textAlignment = NSTextAlignmentCenter;
+            [self.view addSubview:pleaseEnterLabel];
+        }
+// TODO // If the user has not entered any text into the UITextField, display in the UILabel, "Username cannot be empty". Otherwise, display "User: username has been logged in".
+    
+//Create a UIButton using the rounded rectangle type. Give this button any color you wish.
+//Add text "Show Date"
+// add action to call onClick add tag to specify button I
+    UIButton *showDate = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (showDate != nil)
+    {
+        showDate.frame = CGRectMake(10.0f, 250.0f, 300.0f, 30.0f);
+        showDate.tintColor = [UIColor redColor];
+        showDate.tag = 1;
+        [showDate setTitle:@"Show Date" forState:UIControlStateNormal];
+        [showDate addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:showDate];
+        
+        
+    }
+    
+//Create a UIButton using either the light or dark info type and position it somewhere near the bottom of the screen.
+//Create a UILabel beneath it that contains no initial text.
+    UIButton *personInfo = [UIButton buttonWithType:UIButtonTypeInfoDark];
+    if(personInfo != nil)
+    {
+        personInfo.frame = CGRectMake(10.0f, 300.0f, 30.0f, 30.0f);
+        [personInfo addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        personInfo.tag = 2;
+        [self.view addSubview:personInfo];
+    }
+    creatorInfo = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 340.0f, 300.0f, 80.0f)];
+    if(creatorInfo != nil)
+    {
+        creatorInfo.numberOfLines = 2;
+        [self.view addSubview:creatorInfo];
+    }
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)onClick:(UIButton*)button
+{
+
+    switch (button.tag) {
+        case 0:
+        {
+            
+            NSString *username = [usernameText text];
+            
+            if (username.length == 0)
+            {
+                pleaseEnterLabel.text = @"Username cannot be empty";
+            }
+            else
+            {
+                NSString *loggedIn = [NSString stringWithFormat: @"User: %@ has been logged in", username];
+                pleaseEnterLabel.text = loggedIn ;
+            }
+        }
+            break;
+        case 1:
+        {
+            NSDateFormatter *theFormatter = [[NSDateFormatter alloc] init];
+                    [theFormatter setDateFormat:@"MMMM d, YYYY hh:mm:ss aaaa zzzz"];
+                    NSString *myDate = [theFormatter stringFromDate:[NSDate date]];
+            
+                    UIAlertView *showMessage = [[UIAlertView alloc]initWithTitle:@"Today's date is:" message:[NSString stringWithFormat:@"%@",myDate] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                    if(showMessage != nil)
+                    {
+                        [showMessage show];
+                   }
+
+        
+        }
+            break;
+        case 2:
+        {
+         creatorInfo.text = @"This application was created by: Joel Johnson";
+        
+        }
+            
+        default:
+            break;
+    }
+
+    
+
 }
 
 - (void)didReceiveMemoryWarning
